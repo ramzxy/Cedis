2299aa04484cc3fe52db94caf771c727c6e4e36a
Ilia Mirzaali <46376329+ramzxy@users.noreply.github.com>
2025-06-19 20:47:21 +0200

    added connection class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 ﻿#include <iostream>
+#include "connection.h"
 #include <boost/asio.hpp>
 
 using namespace boost::asio;
 
 int main() {
+	std::string server_ip = "127.0.0.1";
+	int server_port = 8090;
+
 	std::cout << "Hello, Boost.Asio!" << std::endl;
-}
+	io_context io_context; 
+
+
+	auto connection = std::make_shared<Connection>(io_context, server_ip, server_port);
+}	
